58f01d2e8407381b2db3c280799906ac0bfc6b7f
ecourse <smkang@codenuri.co.kr>
2021-11-05 13:25:59 +0900

    Add files via upload

---
+++ CPP/5_STL_ALGORITHM2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// Step 2. 검색 구간의 일반화 ( 부분 문자열 검색이 가능하게)
+char* strchr(char* first, char* last, int c)
+{
+	while ( first != last && *first != c)
+		++first;
+
+	return *first == 0 ? nullptr : first;
+}
+
+int main()
+{
+	char s[] = "abcdefgh";
+
+	char* p = strchr(s, s+4, 'e');
+
+	if (p == nullptr)
+		std::cout << "not found" << std::endl;
+	else
+		std::cout << "found : " << *p << std::endl;
+}
